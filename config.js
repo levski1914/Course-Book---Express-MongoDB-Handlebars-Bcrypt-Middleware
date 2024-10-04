51fd1e90cafd5e207f4b1a7c5ff3dce26f46927b
Georgi Dimitrov <austria1233@abv.bg>
2024-10-04 17:09:45 +0300

    enter

--- config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  SECRET: "9182347asduoi798sdf89saiofuyh83729dfuhisfahiu3",
-};
--- utils/errorUtils.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-exports.getErrorMessage = (err) => {
-  if (err instanceof mongoose.MongooseError) {
-    return Object.values(err.errors).at(0).message;
-  } else if (err instanceof Error) {
-    return err.message;
-  }
-};
